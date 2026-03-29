01a9918ea805258ef102a055fed748526d444595
rajibul2004 <rajibulhazari786@gmail.com>
2026-03-29 22:11:56 +0530

    MongoDB Connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/mongodb.js';
 
 dotenv.config();
 
@@ -8,8 +9,9 @@ const HOST = process.env.HOST || 'localhost';
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express();
-
+connectDB();
 app.use(express.json({ limit: '10mb' }));
+
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://${HOST}:${PORT}/`);
